0b3343467519b8bd5e2dfed0028a1c5a6f9928e0
kengo nakajima <kengo.nakajima@gmail.com>
2025-09-13 08:09:47 +0900

    sloc

---
+++ sloc.rb
@@ -0,0 +1,177 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Simple SLOC counter for this repository.
+# - Counts non-blank, non-comment lines.
+# - Supports C/C++-style (//, /* */) and hash-style (#) comments.
+# - Scans current directory by default; pass a path as the first argument.
+# - Excludes common dot-directories and debug/binary outputs.
+
+require 'find'
+require 'optparse'
+
+# Language groups by comment style
+C_STYLE_EXTS = %w[
+  c cc cpp cxx h hpp hh inl inc m mm
+  js jsx ts tsx java kt cs go rs swift php proto
+].freeze
+
+HASH_STYLE_EXTS = %w[
+  rb py sh bash zsh fish
+].freeze
+
+# Special filenames (no extension) treated as code
+SPECIAL_HASH_FILES = %w[
+  Makefile Rakefile Gemfile
+].freeze
+
+DEFAULT_EXCLUDE_DIRS = [
+  '.git', '.hg', '.svn', '.vscode', '.idea', '.venv',
+  'node_modules', 'dist', 'build', 'out', 'target', 'tmp'
+].freeze
+
+def style_for(path)
+  base = File.basename(path)
+  return :hash if SPECIAL_HASH_FILES.include?(base)
+
+  ext = File.extname(base).downcase.sub(/^\./, '')
+  return :c if C_STYLE_EXTS.include?(ext)
+  return :hash if HASH_STYLE_EXTS.include?(ext)
+
+  nil
+end
+
+def text_lines(path)
+  data = File.binread(path)
+  text = data.encode('UTF-8', invalid: :replace, undef: :replace, replace: '')
+  text.split(/\r\n|\r|\n/)
+rescue => e
+  warn "[warn] cannot read #{path}: #{e}"
+  []
+end
+
+def count_sloc_c_style(lines)
+  sloc = 0
+  in_block = false
+
+  lines.each do |line|
+    l = line.dup
+
+    # Remove block comments possibly spanning multiple segments in the same line
+    loop do
+      if in_block
+        end_idx = l.index('*/')
+        if end_idx
+          l = l[(end_idx + 2)..-1] || ''
+          in_block = false
+          next # continue scanning this line for more blocks
+        else
+          l = ''
+          break
+        end
+      end
+
+      # If a line comment '//' appears before a new '/*', trim at '//'
+      line_idx = l.index('//')
+      block_idx = l.index('/*')
+
+      if block_idx && (!line_idx || block_idx < line_idx)
+        l = l[0...block_idx]
+        in_block = true
+        # continue loop to try to find the block terminator
+        next
+      else
+        # No block start before '//' or no block start at all: drop line comment
+        l = l[0...line_idx] if line_idx
+        break
+      end
+    end
+
+    sloc += 1 unless l.strip.empty?
+  end
+
+  sloc
+end
+
+def count_sloc_hash_style(lines)
+  lines.count do |line|
+    s = line.strip
+    !(s.empty? || s.start_with?('#'))
+  end
+end
+
+def count_file(path, style)
+  lines = text_lines(path)
+  case style
+  when :c
+    count_sloc_c_style(lines)
+  when :hash
+    count_sloc_hash_style(lines)
+  else
+    0
+  end
+end
+
+def excluded_dir?(path)
+  base = File.basename(path)
+  return true if base.start_with?('.') && base != '.' && base != '..'
+  return true if DEFAULT_EXCLUDE_DIRS.include?(base)
+  return true if base.end_with?('.dSYM')
+  false
+end
+
+options = {
+  root: ARGV[0] || '.',
+  details: true,
+}
+
+OptionParser.new do |opt|
+  opt.banner = 'Usage: ruby sloc.rb [PATH]'
+  opt.on('--no-details', 'Do not show per-extension breakdown') { options[:details] = false }
+end.parse!(ARGV[1..] || [])
+
+root = options[:root]
+unless File.directory?(root)
+  warn "[error] not a directory: #{root}"
+  exit 1
+end
+
+per_ext = Hash.new { |h, k| h[k] = { files: 0, sloc: 0 } }
+total_files = 0
+total_sloc = 0
+
+Find.find(root) do |path|
+  if File.directory?(path)
+    if excluded_dir?(path)
+      Find.prune
+    end
+    next
+  end
+
+  st = style_for(path)
+  next unless st
+
+  sloc = count_file(path, st)
+  next if sloc <= 0
+
+  ext = File.extname(path).downcase.sub(/^\./, '')
+  ext = 'no-ext' if ext.empty?
+  per_ext[ext][:files] += 1
+  per_ext[ext][:sloc] += sloc
+  total_files += 1
+  total_sloc += sloc
+end
+
+if options[:details]
+  # Sort by SLOC desc
+  width = per_ext.keys.map(&:length).max.to_i
+  puts 'SLOC by extension:'
+  per_ext.sort_by { |_k, v| -v[:sloc] }.each do |ext, v|
+    puts format("  %-#{[width, 3].max}s  files:%5d  sloc:%8d", ext, v[:files], v[:sloc])
+  end
+  puts
+end
+
+puts format('Total files: %d', total_files)
+puts format('Total SLOC : %d', total_sloc)
+
