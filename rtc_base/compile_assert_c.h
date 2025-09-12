8bebc59761cf30899de2609e854ae011ac3811ba
kengo nakajima <kengo.nakajima@gmail.com>
2025-09-13 08:09:19 +0900

    rm

--- rtc_base/compile_assert_c.h
+++ rtc_base/compile_assert_c.h
@@ -1,25 +1,20 @@
-/*
- *  Copyright (c) 2012 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- */
-
+/* Minimal compile-time assert macros for C and C++. */
 #ifndef RTC_BASE_COMPILE_ASSERT_C_H_
 #define RTC_BASE_COMPILE_ASSERT_C_H_
 
-// Use this macro to verify at compile time that certain restrictions are met.
-// The argument is the boolean expression to evaluate.
-// Example:
-//   RTC_COMPILE_ASSERT(sizeof(foo) < 128);
-// Note: In C++, use static_assert instead!
-#define RTC_COMPILE_ASSERT(expression) \
-  switch (0) {                         \
-    case 0:                            \
-    case expression:;                  \
-  }
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L)
+#define RTC_COMPILE_ASSERT(expr) _Static_assert((expr), "RTC_COMPILE_ASSERT")
+#else
+#define RTC_COMPILE_ASSERT(expr) typedef char rtc_compile_assert_failed[(expr) ? 1 : -1]
+#endif
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
 
 #endif  // RTC_BASE_COMPILE_ASSERT_C_H_
+
