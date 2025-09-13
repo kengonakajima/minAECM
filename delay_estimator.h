e63a62ee07f8b82f405a3bab081538d60c05a435
kengo nakajima <kengo.nakajima@gmail.com>
2025-09-13 11:12:05 +0900

    rm

--- delay_estimator.h
+++ delay_estimator.h
@@ -1,13 +1,3 @@
-/*
- *  Copyright (c) 2012 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree. An additional intellectual property rights grant can be found
- *  in the file PATENTS.  All contributing project authors may
- *  be found in the AUTHORS file in the root of the source tree.
- */
-
 // Performs delay estimation on binary converted spectra.
 // The return value is  0 - OK and -1 - Error, unless otherwise stated.
 
